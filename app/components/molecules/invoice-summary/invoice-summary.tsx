592e595027e4b9398b3e530f0ccb1c2180f6e95c
Alex Marshall <alex.k.marshall83@gmail.com>
2022-06-12 17:39:21 +0100

    fix: gap on invoice summary

--- app/components/molecules/invoice-summary/invoice-summary.tsx
+++ app/components/molecules/invoice-summary/invoice-summary.tsx
@@ -55,7 +55,7 @@ export function InvoiceSummary({
         'flex max-w-screen-md cursor-pointer flex-col gap-6 rounded-md bg-white p-6 outline outline-2 outline-transparent',
         'dark:bg-slate-800',
         'focus-within:outline-violet-600 hover:outline-violet-600',
-        'gap-10 sm:flex-row sm:items-baseline sm:py-4'
+        'sm:flex-row sm:items-baseline sm:gap-10 sm:py-4'
       )}
       {...invoiceClickProps}
     >
