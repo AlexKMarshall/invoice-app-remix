683d89b0b0f36391a929072449e36c098276d56b
Alex Marshall <alex.k.marshall83@gmail.com>
2022-06-12 17:26:05 +0100

    style: finesse invoice sizing on larger screens

---
+++ app/components/atoms/icons/arrow-right.tsx
@@ -0,0 +1,8 @@
+import { createIcon } from '~/components/atoms/icon'
+
+export const ArrowRightIcon = createIcon(
+  <path d="M1.5 1l4 4-4 4" strokeWidth="2" />,
+  'ArrowRightIcon',
+  '0 0 7 10',
+  'stroke'
+)
