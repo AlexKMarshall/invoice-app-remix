f3fa50a95cfcda0b7feb7140555410fcb778dfb4
Alex Marshall <alex.k.marshall83@gmail.com>
2022-06-20 18:41:50 +0100

    style: add margin bottom prop to input

--- app/components/atoms/input/input.tsx
+++ app/components/atoms/input/input.tsx
@@ -7,6 +7,7 @@ type Props = {
   label: string
   errorMessage?: string
   colSpan?: ResponsiveColSpan
+  mb?: ResponsiveMarginBottom
 } & Except<
   InputHTMLAttributes<HTMLInputElement>,
   'className' | 'aria-invalid' | 'aria-errormessage'
@@ -16,6 +17,7 @@ export function Input({
   errorMessage,
   id,
   colSpan,
+  mb,
   ...props
 }: Props): JSX.Element {
   const generatedId = useId()
@@ -24,9 +26,10 @@ export function Input({
   return (
     <div
       className={clsx(
-        'flex flex-col gap-2',
+        'flex flex-1 flex-col gap-2',
         props.disabled ? 'opacity-50' : '',
-        colSpan
+        colSpan,
+        mb
       )}
     >
       <div
@@ -46,7 +49,7 @@ export function Input({
         aria-invalid={errorMessage ? true : undefined}
         aria-errormessage={errorMessageId}
         className={clsx(
-          'rounded-sm bg-gray-50 px-5 py-4 font-bold text-strong caret-violet-600 outline outline-2 outline-regular/40',
+          'w-full min-w-0 rounded-sm bg-gray-50 px-5 py-4 font-bold text-strong caret-violet-600 outline outline-2 outline-regular/40',
           'focus-visible:outline-violet-600',
           'dark:bg-gray-800 dark:outline-transparent dark:focus-visible:outline-violet-600',
           'aria-invalid:outline-red-600 aria-invalid:dark:outline-red-500'
@@ -73,4 +76,14 @@ type ColSpan =
 
 type BreakPoint = '' | 'sm:' | 'md:' | 'lg:'
 
-type ResponsiveColSpan = `${BreakPoint}${ColSpan}`
+type Responsive<T extends string> = `${BreakPoint}${T}`
+
+type ResponsiveColSpan = Responsive<ColSpan>
+
+const spaceScale = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 16, 20, 24, 28, 32, 36, 40, 44,
+  48, 52, 56, 60, 64, 72, 80, 96,
+] as const
+type Space = typeof spaceScale[number]
+type MarginBottom = `mb-${Space}`
+type ResponsiveMarginBottom = Responsive<MarginBottom>
