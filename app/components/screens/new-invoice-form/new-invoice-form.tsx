49f239f99367a099e46935f643b26f022efe1d06
Alex Marshall <alex.k.marshall83@gmail.com>
2022-06-20 13:15:44 +0100

    refactor: remove unneeded ids from form;

--- app/components/screens/new-invoice-form/new-invoice-form.tsx
+++ app/components/screens/new-invoice-form/new-invoice-form.tsx
@@ -9,42 +9,25 @@ export function NewInvoiceForm({ Link = RemixLink }: Props): JSX.Element {
   return (
     <form>
       <h2>Bill From</h2>
-      <Input
-        label="Street Address"
-        id="from-street-address"
-        name="fromStreetAddress"
-      />
-      <Input label="City" id="from-city" name="fromCity" />
-      <Input label="Post Code" id="from-post-code" name="fromPostCode" />
-      <Input label="Country" id="from-country" name="fromCountry" />
+      <Input label="Street Address" name="fromStreetAddress" />
+      <Input label="City" name="fromCity" />
+      <Input label="Post Code" name="fromPostCode" />
+      <Input label="Country" name="fromCountry" />
       <h2>Bill To</h2>
-      <Input label="Client's Name" id="client-name" name="clientName" />
-      <Input
-        label="Street Address"
-        id="to-street-address"
-        name="toStreetAddress"
-      />
-      <Input label="City" id="to-city" name="toCity" />
-      <Input label="Post Code" id="to-post-code" name="toPostCode" />
-      <Input label="Country" id="to-country" name="toCountry" />
+      <Input label="Client's Name" name="clientName" />
+      <Input label="Street Address" name="toStreetAddress" />
+      <Input label="City" name="toCity" />
+      <Input label="Post Code" name="toPostCode" />
+      <Input label="Country" name="toCountry" />
 
-      <Input
-        label="Invoice Date"
-        id="invoice-date"
-        name="invoiceDate"
-        type="date"
-      />
-      <Input label="Payment Terms" id="payment-terms" name="paymentTerms" />
-      <Input
-        label="Project Description"
-        id="project-description"
-        name="projectDescription"
-      />
+      <Input label="Invoice Date" name="invoiceDate" type="date" />
+      <Input label="Payment Terms" name="paymentTerms" />
+      <Input label="Project Description" name="projectDescription" />
 
       <h2>Item list</h2>
-      <Input label="Item Name" id="item-name" name="itemName" />
-      <Input label="Qty." id="item-quantity" name="itemQuantity" />
-      <Input label="Price" id="item-price" name="itemPrice" />
+      <Input label="Item Name" name="itemName" />
+      <Input label="Qty." name="itemQuantity" />
+      <Input label="Price" name="itemPrice" />
       <Button
         color="secondary"
         as={Link}
